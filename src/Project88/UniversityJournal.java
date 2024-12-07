afce9218657e2fa4cfd25826bb1715b29d052a9f
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-12-07 15:49:00 +0500

    Update UniversityJournal.java

--- src/Project88/UniversityJournal.java
+++ src/Project88/UniversityJournal.java
@@ -1,12 +1,17 @@
 package Project88;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UniversityJournal {
     private String name;
     private String publisher;
+    private List<ResearchPaper> papers;
 
     public UniversityJournal(String name, String publisher) {
         this.name = name;
         this.publisher = publisher;
+        this.papers = new ArrayList<>();
     }
 
     public String getName() {
@@ -23,5 +28,19 @@ public class UniversityJournal {
 
     public void setPublisher(String publisher) {
         this.publisher = publisher;
+    }
+
+    public List<ResearchPaper> getPapers() {
+        return new ArrayList<>(papers);
+    }
+
+    public void addPaper(ResearchPaper paper) {
+        if (!papers.contains(paper)) {
+            papers.add(paper);
+        }
+    }
+
+    public void removePaper(ResearchPaper paper) {
+        papers.remove(paper);
     }
 }
