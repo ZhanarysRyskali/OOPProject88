e1d9c676d42a6469e08cfd9080c89e3032b38b49
ZhanarysRyskali <156776852+ZhanarysRyskali@users.noreply.github.com>
2024-12-13 16:56:17 +0600

    Update Order.java

--- src/Project88/Order.java
+++ src/Project88/Order.java
@@ -1,5 +1,8 @@
 package Project88;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
     private int orderId;
     private String description;
@@ -33,5 +36,39 @@ public class Order {
 
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    public boolean isNew() {
+        return this.status == Status.PENDING;
+    }
+
+    public static List<Order> filterOrders(List<Order> orders, Status status) {
+        List<Order> filteredOrders = new ArrayList<>();
+        for (Order order : orders) {
+            if (order.getStatus() == status) {
+                filteredOrders.add(order);
+            }
+        }
+        return filteredOrders;
+    }
+
+    public static List<Order> getAcceptedNotDoneOrders(List<Order> orders) {
+        List<Order> filteredOrders = new ArrayList<>();
+        for (Order order : orders) {
+            if (order.getStatus() == Status.ACCEPTED) {
+                filteredOrders.add(order);
+            }
+        }
+        return filteredOrders;
+    }
+
+    public static List<Order> getDoneOrders(List<Order> orders) {
+        List<Order> filteredOrders = new ArrayList<>();
+        for (Order order : orders) {
+            if (order.getStatus() == Status.COMPLETED) {
+                filteredOrders.add(order);
+            }
+        }
+        return filteredOrders;
     }
 }
