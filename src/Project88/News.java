50577eaff2c770e0886da8b8daa662054a0af963
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-12-13 15:57:28 +0500

    Update News.java

--- src/Project88/News.java
+++ src/Project88/News.java
@@ -1,6 +1,7 @@
 package Project88;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class News {
@@ -8,8 +9,14 @@ public class News {
 	private String content;
 	private String topic;
 	private List<String> comments;
+	private List<News> newsList;
+	private boolean pinned;
 
-	public News() {
+	public News(String title, String content, String topic) {
+		this.title = title;
+		this.content = content;
+		this.topic = topic;
+		this.pinned = "Research".equalsIgnoreCase(topic);
 		this.comments = new ArrayList<>();
 	}
 
@@ -39,9 +46,42 @@ public class News {
 
 	public void setTopic(String topic) {
 		this.topic = topic;
+		this.pinned = "Research".equalsIgnoreCase(topic);
+	}
+
+	public boolean isPinned() {
+		return pinned;
+	}
+
+	private void prioritizeNews() {
+		newsList.sort(Comparator.comparing(News::isPinned).reversed());
+	}
+
+	public void addNews(News news) {
+		newsList.add(news);
+		prioritizeNews();
+	}
+
+	public void announcePaper(ResearchPaper paper) {
+		News announcement = new News(
+				"New Research Paper Published!",
+				String.format(
+						"A new research paper titled '%s' has been published in the journal '%s'.",
+						paper.getTitle(),
+						paper.getJournal()
+				),
+				"Research"
+		);
+		addNews(announcement);
 	}
 
 	public List<String> getComments() {
 		return new ArrayList<>(comments);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Title: %s\nTopic: %s\nContent: %s\nComments: %s",
+				title, topic, content, comments.isEmpty() ? "No comments yet." : comments);
 	}
 }
