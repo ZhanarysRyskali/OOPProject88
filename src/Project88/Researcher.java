d8a84bcd1327b56862fcec0c4514629b42a8387e
ZhanarysRyskali <156776852+ZhanarysRyskali@users.noreply.github.com>
2024-12-18 03:03:31 +0600

    Update Researcher.java

--- src/Project88/Researcher.java
+++ src/Project88/Researcher.java
@@ -1,20 +1,21 @@
+
 package Project88;
 
 import java.util.Comparator;
 import java.util.List;
 
 public interface Researcher {
-	String getResearchArea();
 
 	List<ResearchProject> getProjects();
 
 	List<ResearchPaper> getPapers();
 
-	int calculateHIndex();
 
 	void joinProject(ResearchProject project);
 
 	void printPapers(Comparator<ResearchPaper> comparator);
 
 	public void publishPaper(ResearchPaper paper, News news);
+
+	double getHIndex();
 }
